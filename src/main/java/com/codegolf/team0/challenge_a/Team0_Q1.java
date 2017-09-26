75fb5637426e2f25aad68067a5a7b58f371648f0
Tin-Pui <tchan15@gmail.com>
2017-09-26 12:04:13 +0100

    First commit for solutions to challenge A and C

---
+++ src/main/java/com/codegolf/team0/challenge_a/Team0_Q1.java
@@ -0,0 +1,59 @@
+package com.codegolf.team0.challenge_a;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tchan on 26/09/17.
+ */
+public class Team0_Q1 {
+
+    public static void main(String[] args) {
+        //testAlgorithmSpeed();
+        //testAlgorithmSpeed();
+    }
+
+    private static void testAlgorithmSpeed() {
+        List<Long> times = new ArrayList<>();
+        for (int i = 1; i<= 30; i++) {
+            long start = System.currentTimeMillis();
+            findPrimeNumber(100000);
+            long finish = System.currentTimeMillis();
+            System.out.println(finish - start + " ms");
+            times.add(finish-start);
+        }
+        long algorithm1 = 0;
+        for (Long time : times) {
+            algorithm1 += time;
+        }
+        System.out.println(findPrimeNumber(100000));
+        System.out.println("average: "+algorithm1/(times.size()) + " ms");
+    }
+
+    public static int findPrimeNumber(int primeIndex) {
+        List<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        Integer currentInteger = 3;
+        boolean currentIntegerIsPrime;
+        do {
+            currentIntegerIsPrime = true;
+            double max = Math.sqrt(currentInteger);
+            for (Integer prime : primes) {
+                if (prime > max){
+                    break;
+                }
+                if (prime <= max && currentInteger%prime==0) {
+                    currentIntegerIsPrime = false;
+                    break;
+                }
+
+            }
+            if (currentIntegerIsPrime) {
+                primes.add(currentInteger);
+            }
+            currentInteger++;
+            currentInteger++;
+        } while (primes.size() < primeIndex);
+        return primes.get(primeIndex-1);
+    }
+}
---
+++ src/main/java/com/codegolf/team0/challenge_c/Team0_Q3.java
@@ -0,0 +1,21 @@
+package com.codegolf.team0.challenge_c;
+
+import java.util.Calendar;
+
+/**
+ * Created by tchan on 26/09/17.
+ */
+public class Team0_Q3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double calculateAge(int day, int month, int year) {
+        Calendar.Builder calendarBuilder = new Calendar.Builder().setDate(year, month, day);
+        long birthTime = calendarBuilder.build().getTimeInMillis();
+        long presentTime = System.currentTimeMillis();
+        long millisecondsInYear = 31556926080L;
+        return (presentTime - birthTime)/ (double)millisecondsInYear;
+    }
+}
