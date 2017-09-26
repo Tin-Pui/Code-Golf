6367d53ef8f4d719280540f20fbcc404deca0fc9
Tin-Pui <tchan15@gmail.com>
2017-09-26 17:26:36 +0100

    Solution for challenge D submitted. Solution requires 1 iteration through the array and assumes that the day trader must end the day with no stock.

---
+++ src/main/java/com/codegolf/team0/challenge_d/Team0_Q4.java
@@ -0,0 +1,45 @@
+package com.codegolf.team0.challenge_d;
+
+/**
+ * Created by tchan on 26/09/17.
+ */
+public class Team0_Q4 {
+
+    public static void main(String[] args) {
+        findBestDayTrade(19.35, 19.30);
+    }
+
+    public static void findBestDayTrade(double... prices) {
+        double bestBuyPrice = 0;
+        double lowestBuyPrice = 0;
+        double bestSellPrice = 0;
+        double maxProfit = 0;
+        boolean buyExists = false;
+        boolean sellExists = false;
+        for (double price : prices) {
+            if (!buyExists) {
+                lowestBuyPrice = price;
+                buyExists = true;
+            } else if (!sellExists) {
+                if (price > lowestBuyPrice) {
+                    bestBuyPrice = lowestBuyPrice;
+                    bestSellPrice = price;
+                    maxProfit = bestSellPrice - bestBuyPrice;
+                    sellExists = true;
+                } else {
+                    lowestBuyPrice = price;
+                }
+            } else {
+                if (price < lowestBuyPrice) {
+                    lowestBuyPrice = price;
+                }
+                if ((price - lowestBuyPrice)>maxProfit) {
+                    bestBuyPrice = lowestBuyPrice;
+                    bestSellPrice = price;
+                    maxProfit = bestSellPrice - bestBuyPrice;
+                }
+            }
+        }
+        System.out.println("Buy at " + bestBuyPrice + " sell at " + bestSellPrice + " for a profit of " + maxProfit);
+    }
+}
